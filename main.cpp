f452d1660cc48fb19bf7d302574eb6c9c72365a9
= <=>
2025-05-14 08:29:43 -0400

    quitting and 2-axis movement

--- main.cpp
+++ main.cpp
@@ -18,25 +18,27 @@ void init(struct Game* game){
   game->keyboard_state = SDL_GetKeyboardState(NULL);
 }
 
-//Should be called every frame of the frame
-void step(struct Game* game){
-    SDL_SetRenderDrawColor(game->renderer, 0xff, 0xff, 0xff, SDL_ALPHA_OPAQUE);
-    SDL_RenderClear(game->renderer);
-    SDL_SetRenderDrawColor(game->renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
-    SDL_RenderFillRect(game->renderer, &game->rectangle);
-    SDL_RenderPresent(game->renderer);
-    SDL_PumpEvents();
-    const bool* keyboard_state = game->keyboard_state;
-    game->rectangle.x += keyboard_state[SDL_SCANCODE_D] - keyboard_state[SDL_SCANCODE_A];
+//Should be called every frame of the frame, returns 0 is quitting
+int step(struct Game* game){
+  SDL_SetRenderDrawColor(game->renderer, 0xff, 0xff, 0xff, SDL_ALPHA_OPAQUE);
+  SDL_RenderClear(game->renderer);
+  SDL_SetRenderDrawColor(game->renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
+  SDL_RenderFillRect(game->renderer, &game->rectangle);
+  SDL_RenderPresent(game->renderer);
+  SDL_PumpEvents();
+  const bool* ks = game->keyboard_state;
+  if(ks[SDL_SCANCODE_ESCAPE]){return 0;}
+  game->rectangle.x += ks[SDL_SCANCODE_D] - ks[SDL_SCANCODE_A];
+  game->rectangle.y += ks[SDL_SCANCODE_S] - ks[SDL_SCANCODE_W];
+  return 1;
 }
 
 int main(){
-  int i;
   struct Game game;
   init(&game);
   //This will eventually be a while loop
-  for(i = 0; i < 200; i++){
-    step(&game);
+  while(1){
+    if(!step(&game)) return 1;
     SDL_Delay(20);
   }
   SDL_Quit();
